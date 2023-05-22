08cd017b4b5260673fbf67a8469303fc5203a5f0
MikhailVladimirovi4 <107101075+MikhailVladimirovi4@users.noreply.github.com>
2023-05-22 11:50:25 +0300

    AddActionPannel

---
+++ Assets/Scripts/Garge/ItemsAdd.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemsAdd : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private Button _bay;
+    [SerializeField] private Button _refuse;
+    [SerializeField] private Button _ok;
+}
--- Assets/Scripts/UI/Office.cs
+++ Assets/Scripts/UI/Office.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -6,25 +7,72 @@ using UnityEngine.UIElements;
 public class Office : MonoBehaviour
 {
     [SerializeField] private Text _coin;
-    [SerializeField] private Text _reputation;
+    [SerializeField] private Text _reputationValue;
     [SerializeField] private Text _garageLevel;
     [SerializeField] private Text _parkingPlace;
     [SerializeField] private Text _freeParkingPlace;
     [SerializeField] private Text _trashTrack;
     [SerializeField] private Text _freeTrashTrack;
     [SerializeField] private Wallet _wallet;
+    [SerializeField] private List<Place> _places;
+
+    private List<TrashTrack> _tracks = new List<TrashTrack>();
+    private int _reputation = 0;
+
+
 
     private void OnEnable()
     {
-        GetParametrs();
-    }
-
-    private void GetParametrs()
-    {
-        _coin.text = Convert.ToString(_wallet.Coints);
+        UpdateValues();
     }
 
     private void OnDisable()
     {
+    }
+
+    public void UpLevel()
+    {
+        Debug.Log("поднять уровень");
+    }
+
+    public void AddTrack()
+    {
+        Debug.Log("поднять track");
+    }
+
+    public void AddPlace()
+    {
+        Debug.Log("поднять place");
+    }
+
+    private void UpdateValues()
+    {
+        _coin.text = Convert.ToString(_wallet.Coints);
+        _parkingPlace.text = Convert.ToString(GetPlaceCount());
+        _trashTrack.text = Convert.ToString(GetTrackCount());
+        _reputationValue.text = Convert.ToString(_reputation);
+    }
+
+    private int GetPlaceCount()
+    {
+        int count = 0;
+
+        foreach (Place place in _places)
+        {
+            if (place.gameObject.activeSelf)
+                count++;
+        }
+
+        return count;
+    }
+
+    private int GetTrackCount()
+    {
+        int count = 0;
+
+        foreach (TrashTrack track in _tracks)
+            count++;
+
+        return count;
     }
 }
